ead832224fd1a6bddd346b78bcab5bf441f6dac1
Simple4H <crescentcxm@gmail.com>
2019-06-17 19:09:14 +0800

    filter的使用

--- src/test/java/com/simple4h/StudyApplicationTests.java
+++ src/test/java/com/simple4h/StudyApplicationTests.java
@@ -1,9 +1,13 @@
 package com.simple4h;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,17 @@ public class StudyApplicationTests {
 
     @Test
     public void contextLoads() {
+        String a = "a";
+        String b = "b";
+        System.out.println(a + b);
     }
+
+    @Test
+    public void stream() {
+        List<Integer> lists = Lists.newArrayList(1, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9);
+        List<Integer> collect = lists.stream().filter(s -> s.equals(9)).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+
 
 }
