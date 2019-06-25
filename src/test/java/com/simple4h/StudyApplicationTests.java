0ab6aca1fa24ffec75aa778d4eaa64603da53fae
Simple4H <crescentcxm@gmail.com>
2019-06-25 11:51:59 +0800

    redis template Set使用

--- src/test/java/com/simple4h/StudyApplicationTests.java
+++ src/test/java/com/simple4h/StudyApplicationTests.java
@@ -1,17 +1,30 @@
 package com.simple4h;
 
 import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.BoundSetOperations;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class StudyApplicationTests {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Test
     public void contextLoads() {
@@ -25,6 +38,27 @@ public class StudyApplicationTests {
         List<Integer> lists = Lists.newArrayList(1, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9);
         List<Integer> collect = lists.stream().filter(s -> s.equals(9)).collect(Collectors.toList());
         System.out.println(collect);
+    }
+
+    @Test
+    public void redisSet() {
+        List<Integer> canSendOrderIds = Lists.newArrayList(1, 2, 3, 4, 5);
+        for (Integer canSendOrderId : canSendOrderIds) {
+            redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+                StringRedisConnection stringRedisConn = (StringRedisConnection) connection;
+                stringRedisConn.sAdd("WEIXIN_TEMPLATE_MSG_SUB_JOB", canSendOrderId.toString());
+                redisTemplate.expire("WEIXIN_TEMPLATE_MSG_SUB_JOB", 2, TimeUnit.HOURS);
+                return null;
+            });
+
+        }
+    }
+
+    @Test
+    public void redisGet() {
+        Set<String> set = redisTemplate.opsForSet().members("WEIXIN_TEMPLATE_MSG_SUB_JOB");
+
+        log.error("set:{}", set);
     }
 
 
